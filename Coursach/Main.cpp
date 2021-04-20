ce504668303dbe8235fe533cfa7f2d2f25cc0329
Алексей Агеев <knukknakiknak@gmail.com>
2021-04-21 00:14:04 +0300

    course1

--- Coursach/Main.cpp
+++ Coursach/Main.cpp
@@ -1,5 +1,5 @@
-//a1e4e860b0eebcfa323fa79c41a9989b4a9dfd95
-//21a56638304140b3099acee719c3ab0b6ebc6c4b
+//knooki     a1e4e860b0eebcfa323fa79c41a9989b4a9dfd95
+//venia11let 21a56638304140b3099acee719c3ab0b6ebc6c4b
 #include "main.h"
 #include "entrance.h"
 #include "user.h"
