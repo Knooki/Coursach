b96c1769be38e921c3b8d087e0c428eb2fbfea29
Knooki <76787340+Knooki@users.noreply.github.com>
2021-04-30 02:49:52 +0300

    Update Classes.h

--- Coursach/Classes.h
+++ Coursach/Classes.h
@@ -1,64 +1,96 @@
 #pragma once
 #include "main.h"
+
 class entrance {
 private:
-	string login, password, * log, * pas;
-	int type;
 	vector<string> _login, _password;
 	vector<int> _type;
+	string* log;
 	int entr_menu();
 	int load_from_file();
-	bool save_in_file(string login, string password, int type);
+	bool save_to_file(string login, string password, int type);
 public:
-	entrance(string* log, string* pas) {
+	entrance(string* log) {
 		this->log = log;
-		this->pas = pas;
 	}
-	entrance() {}
-	void change_pas(string* login, string* password);
+	void change_pas(string* login);
 	int entering();
 };
 
 class admin {
 private:
-	string* login, * password;
+	string* login;
 	int menu_admin();
 	void add_data();
 	void change_data();
 	void delete_data();
+	void sort_data();
 	void user_manage();
 public:
-	admin(string* login, string* password) {
+	admin(string* login) {
 		this->login = login;
-		this->password = password;
 	}
-	admin() {}
-	void three_subjects();//Вывести на экран 3 пред-мета по которым больше всего отрицательных оценок
-	void show_info();
-	void search();
 	void operation();
 };
 
 class user {
 private:
-	//сделай счетчик с помощью static
-	string* login, * password;
+	string* login;
 	int menu_user();
 public:
-	user(string* login, string* password) {
+	user(string* login) {
 		this->login = login;
-		this->password = password;
 	}
 	void operation();
 };
 
-class subject {
-private:
-	int code;
+struct subject {
+	int code_of_subject;
 	string name;
 	string teacher_name;
 	int hours;
-	int semester;
-public:
-	subject() {}
+	int number_of_semester;
+	vector<subject> load_from_file();
+	vector<subject> sort_array(vector<subject> arr);
+	void save_to_file(subject new_subj);
+	void change_data_in_file(vector<subject> array, string type_sort);
+	void show_info_subj(string sort_type);
+	void add_subj();
+	void change_subj();
+	void delete_subj_or_sort_subj(string type);
+};
+
+struct student {
+	int code_of_student;
+	string full_name;
+	string birth_date;
+	string speciality;
+	string group;
+	int course;
+	vector<student> load_from_file();
+	vector<student> sort_array(vector<student> array);
+	vector<student> sort_array_of_accounting(vector<student> array_of_stud);
+	void save_to_file(student new_stud);
+	void change_data_in_file(vector<student> array, string type_sort);
+	void show_info_stud(string sort_type);
+	void add_stud();
+	void change_stud();
+	void delete_stud_or_sort_stud(string type);
+};
+
+struct accounting {
+	student st;
+	subject sub;
+	string date;
+	int mark;
+	int code_of_acc;
+	vector<accounting> load_from_file();
+	void save_to_file(accounting new_acc);
+	void change_data_in_file(vector<accounting> array, string type_sort);
+	void show_info(string sort_type);
+	void add_info();
+	void change_info();
+	void delete_info_or_sort_info(string type);
+	void show_info_about_three();
+	void delete_all_info_about_student(int code_of_student);
 };
